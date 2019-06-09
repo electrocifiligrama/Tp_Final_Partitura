15d83f2eedbec3778534ebaac13fc3a50f990f40
EzequielVijande <evijande@itba.edu.ar>
2019-06-09 00:49:10 -0300

    Estuve probando tocando os parametros

--- BPM_detector/BPM_detector/BPM_FFT.py
+++ BPM_detector/BPM_detector/BPM_FFT.py
@@ -4,10 +4,10 @@ import scipy.signal as sig
 from matplotlib import pyplot as plt
 MINIMUM_F= 1
 MAXIMUM_F = 3
-def BPM_estimate(buffer,f_s=44100,n_samples=1024,alpha=0.85):
+def BPM_estimate(buffer,f_s=44100,n_samples=280,alpha=0.8):
     buffer_size = len(buffer)
     number_of_frames = math.ceil( buffer_size/n_samples )
-    n_fft = int(10*number_of_frames)
+    n_fft = int(2*number_of_frames)
     if( n_fft % 2): #Si n es impar entonces la r_fft devuelve (n+1)/2 valores
         rfft_size = (n_fft+1)/2
     else:
@@ -15,6 +15,7 @@ def BPM_estimate(buffer,f_s=44100,n_samples=1024,alpha=0.85):
     frame = buffer[0:n_samples]
     power = np.zeros(number_of_frames)
     power[0] = (1-alpha)*sig.fftconvolve(frame,frame[:-1])[0]
+    power[0] /= n_samples
     for i in range(1,number_of_frames):
         start_index = i*n_samples
         stop_index = (i+1)*n_samples
@@ -23,6 +24,7 @@ def BPM_estimate(buffer,f_s=44100,n_samples=1024,alpha=0.85):
         frame = buffer[start_index:stop_index]
         #Calculo la potencia
         frame_average = sig.fftconvolve(frame,frame[:-1])[0]
+        frame_average /= len(frame)
         power[i] = alpha*(power[i-1]) + (1-alpha)*frame_average
     fs_nueva = f_s/n_samples    
     power_spectrum = np.abs( np.fft.rfft(power,n=n_fft) )
@@ -30,8 +32,8 @@ def BPM_estimate(buffer,f_s=44100,n_samples=1024,alpha=0.85):
     #plt.plot(f,power_spectrum)
     #plt.show()
     freq_res = fs_nueva/(n_fft)
-    bin_start = int(MINIMUM_F/freq_res)
-    bin_stop = int(MAXIMUM_F/freq_res)
+    bin_start = int( round(MINIMUM_F/freq_res) )
+    bin_stop = int( round(MAXIMUM_F/freq_res) )
     bin_max = bin_start + np.argmax(power_spectrum[bin_start:bin_stop])
     f_max = bin_max*freq_res
 
--- BPM_detector/BPM_detector/BPM_detector.py
+++ BPM_detector/BPM_detector/BPM_detector.py
@@ -3,14 +3,17 @@ from matplotlib import pyplot as plt
 import numpy as np
 import OSS_generator as o_gen
 import BPM_FFT as b
+from scipy import stats
 AUDIO_PATH = ".\\Audios"
 
-file_path = AUDIO_PATH+"\\146_BPM_bumblebee.wav"
+file_path = AUDIO_PATH+"\\87_BPM_Monk.wav"
 f_s, audio = wav.read(file_path)
 number_of_audios = 1
 if ( len(audio.shape) > 1):
     number_of_audios = 2
-chunk_size = 10*44100
+samples = 280
+chunk_size = 2048*samples
+alpha = 0.85
 bpm1 = [] #Bpm para el primer audio 
 bpm2 = [] #Bpm del segundo audio (Solo se utiliza para audios en stereo)
 counter = 0
@@ -22,19 +25,29 @@ while counter < len(audio):
         for i in range(0,number_of_audios):
             buffer = audio[counter:stop,i]
             if i >0:
-                bpm2.append( b.BPM_estimate(buffer,f_s,n_samples=512) )
+                bpm2.append( b.BPM_estimate(buffer,f_s,samples,alpha) )
             else:
-                bpm1.append( b.BPM_estimate(buffer,f_s,n_samples=512) )
+                bpm1.append( b.BPM_estimate(buffer,f_s,samples,alpha) )
     else:
         buffer = audio[counter:stop]
-        bpm1.append( b.BPM_estimate(buffer,f_s,n_samples=512) )
+        bpm1.append( b.BPM_estimate(buffer,f_s,samples,alpha) )
     counter += chunk_size
 
 fig = plt.figure()
+if number_of_audios<1:
+    t= np.linspace(start=0,stop=(len(audio)/f_s),num=len(bpm1) )
+else:
+    t= np.linspace(start=0,stop=(audio.shape[0]/f_s),num=len(bpm1) )
 plt.subplot(211)
-plt.plot(bpm1)
+plt.plot(t,bpm1)
 if number_of_audios > 1:
     plt.subplot(212)
-    plt.plot(bpm2)
+    plt.plot(t,bpm2)
 plt.show()
-
+for i in range(0,len(bpm1)):
+    bpm1[i] = int(round(bpm1[i]))
+    #bpm2[i] = int(round(bpm2[i]))
+mode1,a =stats.mode(bpm1,axis=None)
+#mode2,a = stats.mode(bpm2,axis=None)
+print(mode1[0])
+#print(mode2[0])
