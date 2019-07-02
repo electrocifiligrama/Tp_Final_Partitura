b732b034291c67629121f32a6430c619f0ef22f0
taomasgonzalez <togonzalez@itba.edu.ar>
2019-07-02 01:44:03 -0300

    Agregue samples. Hay porblema con el onset detection! detecta muchas veces lo mismo como si fueran notas distintas

---
+++ mainProgram/Sampleador.py
@@ -0,0 +1,37 @@
+from wav_gen import WaveManagement
+from scipy.io import wavfile as wav
+import segmentAlgorithms as SDA
+import PDA as PDA
+
+wav_man = WaveManagement()
+SAMPLES_PATH = ".\\Samples"
+
+
+def trim_wavs(data, note_segments, fs, notes_name):
+    repeated_names = []
+    for i in range(len(note_segments)):
+        aux = data[note_segments[i][0]-5000:note_segments[i][1]+5000]
+        name = notes_name[i]
+        while name in repeated_names:
+            name = name + "a"
+        repeated_names.append(name)
+        wav_man.generate_wav(finished=True, data=aux, n_channels=1, sample_width=2,
+                             frame_rate=fs, file_name=SAMPLES_PATH + "\\" + name+'.wav')
+
+fileName = "samples"
+filePath = fileName + ".wav"
+
+fs, audio = wav.read(filePath)
+audioMono = audio
+
+# Se detectan los intervalos de tiempo donde predominan las notas musicales
+noteSegments = SDA.notesSegmentation(audioMono, fs, SDA.HFC)
+
+# Se averigua el pitch de las notas de cada intervalo hallado
+notesFo = PDA.assignPitch(audioMono, fs, noteSegments, PDA.autocorrelationAlgorithm)
+
+# A partir de la frecuencia fundamental de cada nota se averigua el nombre de las mismas
+notesName = PDA.translateNotes(notesFo)
+
+
+trim_wavs(audioMono, noteSegments, fs, notesName)
---
+++ mainProgram/wav_gen.py
@@ -0,0 +1,32 @@
+import wave
+import math
+import struct
+
+
+class WaveManagement:
+    number_of_bytes_codification = 2        # number of bytes a frame has
+
+    def __init__(self):
+        self.opened_file = None
+
+    # https://soledadpenades.com/posts/2009/fastest-way-to-generate-wav-files-in-python-using-the-wave-module/
+    def generate_wav(self, finished: bool, data: list, n_channels: int = 1, sample_width=2, frame_rate=44100, file_name='NEW_WAV.wav'):
+        """generate_wav generates a new .wav file based on the input data.
+    #The amplitude of each data sample should be a float belonging to the interval [-1,1],
+    #where 1 will be the maximum representable number on the wav file."""
+        if self.opened_file is None:
+            self.opened_file = wave.open(file_name, 'wb')
+            self.opened_file.setparams((n_channels, sample_width, frame_rate, len(data), 'NONE', 'not compressed'))
+        translated_data = []
+
+        # https://docs.python.org/2/library/struct.html#struct-format-strings
+        # h : 2 bytes -> 2^15-1 =32767, maximum amplitude
+        # i : 4 bytes
+        for d in data:
+            translated_data.append(struct.pack('h', int(d)))
+
+        self.opened_file.writeframes(b''.join(translated_data))
+        if finished:
+            self.opened_file.close()
+            self.opened_file = None
+
