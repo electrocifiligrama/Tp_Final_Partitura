f15f945b310adb24270c90ddcfbe52c5f813e6d6
EzequielVijande <evijande@itba.edu.ar>
2020-02-05 23:58:52 -0300

    funca el detector de bpm

--- BPM_detector/BPM_detector/BPM_FFT.py
+++ BPM_detector/BPM_detector/BPM_FFT.py
@@ -7,22 +7,23 @@ MAXIMUM_F = 3
 
 class BPM_Detctor(object):
     """Clase que se encarga de detectar el BPM de una cancion"""
-    def __init__(self, number_of_frames =2048):
+    def __init__(self, number_of_frames =2048,f_s=44100,alpha_ =0.8):
         self.power = np.zeros(number_of_frames)
         self.nfft = number_of_frames
         #Circular array indexes
         self.curr = 0
         self.overflow = False
+        self.fs = f_s
+        self.alpha = alpha_
 
-    def BPM_estimate(self, buffer,f_s=44100,alpha=0.8):
+    def BPM_estimate(self, buffer):
         buffer_size = len(buffer)
-        frame = buffer
         if( self.overflow):
             self.power = np.delete(self.power,0)
             self.power = np.append(self.power,0)
-        average = ( sig.fftconvolve(frame,frame[::-1])[0] )/ buffer_size
-        self.power[self.curr] = alpha*(self.power[self.curr - 1]) + (1-alpha)*average
-        fs_nueva = f_s/buffer_size    
+        average = ( sig.fftconvolve(buffer,buffer[::-1])[0] )/ buffer_size
+        self.power[self.curr] = self.alpha*(self.power[self.curr - 1]) + (1-self.alpha)*average
+        fs_nueva = self.fs/buffer_size    
         power_spectrum = np.abs( np.fft.rfft(self.power,n=self.nfft) )
         #f = np.linspace(start=0,stop=fs_nueva/2.0,num=len( power_spectrum))
         #plt.plot(f,power_spectrum)
--- BPM_detector/BPM_detector/BPM_detector.py
+++ BPM_detector/BPM_detector/BPM_detector.py
@@ -4,23 +4,24 @@ import numpy as np
 import OSS_generator as o_gen
 import BPM_FFT as b
 from scipy import stats
-AUDIO_PATH = ".\\Audios"
+AUDIO_PATH = ".\\Audios\\"
 
-file_path = AUDIO_PATH+"\\06-RunToTheHills.wav"
+file_name = "87_BPM_Monk.wav"
+file_path = AUDIO_PATH + file_name
 f_s, audio = wav.read(file_path)
 number_of_audios = 1
 if ( len(audio.shape) > 1):
     number_of_audios = 2
 samples = 375
-n_fft =2048
+n_fft = 1024
 alpha = 0.85
 bpm1 = [] #Bpm para el primer audio 
 bpm2 = [] #Bpm del segundo audio (Solo se utiliza para audios en stereo)
 counter = 0
 bpm = 0
-detector = b.BPM_Detctor( n_fft )
+detector = b.BPM_Detctor( n_fft, f_s, alpha )
 
-desired_time = 5 #Hasta que segundo de la cancion se quiere sacar el bpm
+desired_time = 20 #Hasta que segundo de la cancion se quiere sacar el bpm
 counter_limit = int( desired_time*f_s )
 
 while counter < counter_limit:
@@ -28,30 +29,24 @@ while counter < counter_limit:
     if stop >= len(audio):
         stop = -1
     if number_of_audios > 1:
-        for i in range(0,number_of_audios):
-            buffer =  audio[counter:stop,i] / max( audio[:,i] ) 
-            if i >0:
-                bpm = detector.BPM_estimate(buffer,f_s,alpha)
-                bpm2.append( bpm )
-            else:
-                bpm = detector.BPM_estimate(buffer,f_s,alpha)
-                bpm1.append( bpm )
+        buffer =  audio[counter:stop,0] / max( audio[:,0] ) 
+        bpm = detector.BPM_estimate(buffer)
+        bpm1.append( bpm )
     else:
         buffer =  audio[counter:stop] / max( audio )
-        bpm = detector.BPM_estimate(buffer,f_s,alpha)
+        bpm = detector.BPM_estimate(buffer)
         bpm1.append( bpm )
     counter += samples
 
 fig = plt.figure()
-if number_of_audios<1:
-    t= np.linspace(start=0,stop=(desired_time),num=len(bpm1) )
-else:
-    t= np.linspace(start=0,stop=(desired_time),num=len(bpm1) )
-plt.subplot(211)
+t= np.linspace(start=0,stop=(desired_time),num=len(bpm1) )
 plt.plot(t,bpm1)
-if number_of_audios > 1:
-    plt.subplot(212)
-    plt.plot(t,bpm2)
+plt.title("BPM de "+file_name)
+plt.xlabel("Tiempo (segundos)")
+plt.ylabel("BPM")
+#if number_of_audios > 1:
+#    plt.subplot(212)
+#    plt.plot(t,bpm2)
 plt.show()
 for i in range(0,len(bpm1)):
     bpm1[i] = int(round(bpm1[i]))
